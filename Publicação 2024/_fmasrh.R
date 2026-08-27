90394b5ea7ee0a3415641b1e2c46b3886ef4607a
Valdson <vcleto@gmail.com>
2026-08-27 19:59:40 -0300

    Padronização dos nomes das bases de dados de RH para serem sempre o nome da base de dados do respectivo equipamento acrescido de "rh" em letras minúsculas.

---
+++ Publicação 2024/_fmasrh.R
@@ -0,0 +1,20 @@
+fmasrh_2018 <- read_excel("../Censo SUAS 2018/12.Fundo Municipal/Censo_SUAS_2018_FM_RH_divulgaç╞o.xlsx") %>%
+  mutate("Ano" = "2018")
+
+fmasrh_2019 <- read_excel("../Censo SUAS 2019/Fundo Municipal/Censo_SUAS_2019_Fundo_Municipal_RH.xls") %>%
+  mutate("Ano" = "2019")
+
+fmasrh_2020 <- read_excel("../Censo SUAS 2020/11_FUNDO_MUNICIPAL/Censo_SUAS_2020_Fundo_Municipal_RH.xlsx") %>%
+  mutate("Ano" = "2020")
+
+fmasrh_2021 <- read_excel("../Censo SUAS 2021/11 - FUNDO MUNICIPAL/Censo_SUAS_2021_Fundo_Municipal_RH.xlsx") %>%
+  mutate("Ano" = "2021")
+
+fmasrh_2022 <- read_excel("../Censo SUAS 2022/11 - FUNDO MUNICIPAL/Censo_SUAS_2022 - Fundo Municipal_RH.xlsx") %>%
+  mutate("Ano" = "2022")
+
+fmasrh_2023 <- read_csv2("../Censo SUAS 2023/11 - FUNDO MUNICIPAL/Censo_SUAS_2023_Fundo_Municipal_RH.csv", locale = locale(encoding = "latin1")) %>%
+  mutate("Ano" = "2023")
+
+fmasrh_2024 <- read_excel("../Censo SUAS 2024/11_FUNDO MUNICIPAL/Censo_SUAS_2024_Fundo_Municipal_RH.xlsx") %>%
+  mutate("Ano" = "2024")
